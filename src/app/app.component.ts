e4514017de0e490dad66e2924efe09e7ec3a69a4
Cleysson <cleysson.anunciacao@sioux.com.br>
2018-01-26 16:04:24 -0200

    Set lang html tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
+import { Component, Inject } from '@angular/core';
+import { Meta, DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
-  constructor (private meta: Meta) {
+  constructor (private meta: Meta, @Inject(DOCUMENT) private document: Document) {
     this.meta.addTag({
       name: 'description',
       content: 'testes de server side com Angular e injeção de meta Tags'
     });
+
+    this.document.documentElement.lang = 'pt-BR';
   }
 }
